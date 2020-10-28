d1a0b67c652f6567216be2b4ba7511c5ded4281f
Eric-Aivaliotis <55474867+Eric-Aivaliotis@users.noreply.github.com>
2020-10-28 19:16:08 -0400

    Add files via upload

---
+++ Assets/_Scripts/BulletController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Util;
+
+public class BulletController : MonoBehaviour
+{
+    public float bulletSpeed = 0.1f;
+    public Boundary boundary;
+
+    //TODO: create a reference to the BulletPoolManager
+
+    public GameObject bullets;
+
+    private BulletPoolManager Bullet_Pool_Manager;
+
+    public int MaxBullets = 0;   //this is for the user to enter how many bullets they want maximun
+
+
+
+    void Start()
+    {
+        boundary.Top = 2.45f;
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        CheckBounds();
+    }
+
+    private void Move()
+    {
+        transform.position += new Vector3(0.0f, bulletSpeed * Time.deltaTime, 0.0f);
+    }
+
+    private void CheckBounds()
+    {
+        if (transform.position.y >= boundary.Top)
+        {
+            //TODO: This code needs to change to use the BulletPoolManager's
+            //TODO: ResetBullet function which will return the bullet to the pool
+
+            Bullet_Pool_Manager.GetBullet();
+            Bullet_Pool_Manager.ResetBullet(bullets);
+           // Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/_Scripts/BulletPoolManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// TODO: Bonus - make this class a Singleton!
+
+[System.Serializable]
+public class BulletPoolManager : MonoBehaviour
+{
+    public GameObject bullet;
+
+    //TODO: create a structure to contain a collection of bullets
+
+    //----------------------
+
+    private GameObject Prefab_Bullet;
+    private Queue<GameObject> BulletPool = new Queue<GameObject>();
+    private int Num_Bullets = 8;    // We will start with 8 bullets for now
+    //------------------------
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // TODO: add a series of bullets to the Bullet Pool
+
+
+        //-------------------------------------
+        for(int slot = 0 ; slot < Num_Bullets  ; slot++ )
+        {
+            bullet = Instantiate(Prefab_Bullet);  
+
+            BulletPool.Enqueue(bullet);      //Adds it to the queue
+
+            bullet.SetActive(false);         //Set for false for now till activated
+        }
+       
+
+
+        //--------------------------------------
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //TODO: modify this function to return a bullet from the Pool
+    public GameObject GetBullet()
+    {
+        if(BulletPool.Count > 0)
+        {
+            bullet = BulletPool.Dequeue();  //Removes it from the queue
+            bullet.SetActive(true);          //Bullet is ready to fire
+            return bullet;
+        }
+
+        else
+
+        {
+            bullet = Instantiate(Prefab_Bullet);
+            return bullet;
+        }
+
+       
+    }
+
+    //TODO: modify this function to reset/return a bullet back to the Pool 
+    public void ResetBullet(GameObject bullet)
+    {
+
+        BulletPool.Enqueue(bullet);  //puts the bullet back
+        bullet.SetActive(false);
+
+    }
+}
